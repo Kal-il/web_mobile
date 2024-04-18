0c47103dfcf01ee6eb4a67e6a9c29535e5f2cffb
Kal-il <kalilgarcia38@gmail.com>
2024-04-17 21:40:22 -0300

    aula 5

--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -121,9 +121,16 @@ DATE_FORMAT = "%d/%m/%Y"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR/'static'   deu erro (pesquisar o o por que depois erro: ?: (staticfiles.E002) The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting.)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/'
--- sistema/sistema/views.py
+++ sistema/sistema/views.py
@@ -1,8 +1,45 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse
+from django.conf import settings
 
 class Login(View):
 
     def get(self, request):
-        contexto = {'mensagem':'login'}
-        return render(request,'autenticacao.html', contexto)
+        
+        contexto = {'mensagem':''}
+        if not request.user.is_authenticated:
+            return HttpResponse('usuario ja está autenticado')
+            #return redirect("/veiculos")
+        else:
+            return render(request, 'autenticacao.html', contexto)
+            
+    # Obtem as credenciais de autenticação do formulário
+    def post(self, request):
+        
+        usuario = request.POST.get('usuario', None)
+        senha = request.POST.get('senha', None)
+        
+        #verifica as credenciais de autenticação fornecidas
+        
+        user = authenticate(request, username=usuario, password=senha)
+        if user is not None:
+            
+            # verifica se
+            
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Usuário autenticado com sucesso')
+            
+                # return redirect("/veiculos")
+                
+            return render(request, 'autenticacao.html', {'mensagem': 'usuario inativo'})
+        return render(request, 'autenticacao.html', {'mensagem': 'usuário ou senha incorretos'} )
+class Logout (View):
+    
+    def get(self, request):
+        imput(request)
+        return request(settings.LOGIN_URL)
